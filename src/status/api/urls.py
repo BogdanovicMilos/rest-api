9dfed18166431d79d45c61b96fa01a1e029387a7
Milos Bogdanovic <milosbogdanovic@Stefans-MacBook-Pro.local>
2019-03-07 19:52:54 +0100

    Added one endpoint for CRUD.

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from .views import StatusAPIView, StatusCreateAPIView, StatusDetailAPIView, StatusUpdateAPIView, StatusDeleteAPIView
+from .views import StatusAPIView  # StatusDetailAPIView, StatusCreateAPIView, StatusUpdateAPIView, StatusDeleteAPIView
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
-    url(r'^create/$', StatusCreateAPIView.as_view()),
-    url(r'^(?P<pk>\d+)/$', StatusDetailAPIView.as_view()),
-    url(r'^(?P<pk>\d+)/update/$', StatusUpdateAPIView.as_view()),
-    url(r'^(?P<pk>\d+)/delete/$', StatusDeleteAPIView.as_view()),
+    # url(r'^create/$', StatusCreateAPIView.as_view()),
+    # url(r'^(?P<pk>\d+)/$', StatusDetailAPIView.as_view()),
+    # url(r'^(?P<pk>\d+)/update/$', StatusUpdateAPIView.as_view()),
+    # url(r'^(?P<pk>\d+)/delete/$', StatusDeleteAPIView.as_view()),
 ]
