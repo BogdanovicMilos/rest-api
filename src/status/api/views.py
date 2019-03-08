a0330569388a7298fe1e77eef1f7125cdd1f0ab0
Milos Bogdanovic <milosbogdanovic@Stefans-MacBook-Pro.local>
2019-03-08 15:38:29 +0100

    View changes.

--- src/status/api/views.py
+++ src/status/api/views.py
@@ -105,21 +105,25 @@ def is_json(json_data):
 #         serializer.save(user=self.request.user)
 
 
-''' Base APIView '''
+''' Base generic APIView '''
 
-# class StatusListSearchAPIView(APIView):
-#     permission_classes = []
-#     authentication_classes = []
 
-#     def get(self, request, format=None):
-#         qs = Status.objects.all()
-#         serializer = StatusSerializer(qs, many=True)
-#         return Response(serializer.data)
+class StatusListSearchAPIView(APIView):
+    permission_classes = []
+    authentication_classes = []
 
-#     def post(self, request, format=None):
-#         qs = Status.objects.all()
-#         serializer = StatusSerializer(qs, many=True)
-#         return Response(serializer.data)
+    def get(self, request, format=None):
+        qs = Status.objects.all()
+        serializer = StatusSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        qs = Status.objects.all()
+        serializer = StatusSerializer(qs, many=True)
+        return Response(serializer.data)
+
+
+''' Mixins View '''
 
 
 class StatusAPIView(mixins.CreateModelMixin, generics.ListAPIView):
@@ -139,23 +143,6 @@ class StatusAPIView(mixins.CreateModelMixin, generics.ListAPIView):
 
     # def perform_create(self, serializer):
     # 	serializer.save(user=self.request.user)
-
-
-# class StatusCreateAPIView(generics.CreateAPIView):
-#     permission_classes = []
-#     authentication_classes = []
-#     queryset = Status.objects.all()
-#     serializer_class = StatusSerializer
-
-#     def get_queryset(self):
-#         qs = Status.objects.all()
-#         query = self.request.GET.get('q')
-#         if query is not None:
-#             qs = qs.filter(content__icontains=query)
-#         return qs
-
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
 
 
 class StatusDetailAPIView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.RetrieveAPIView):
@@ -183,6 +170,45 @@ class StatusDetailAPIView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, gen
     #     if instance is not None:
     #         return instance.delete()
     #     return None
+
+
+''' generic API Views '''
+
+
+# class StatusAPIView(generics.ListAPIView):
+#     permission_classes = []
+#     authentication_classes = []
+#     serializer_class = StatusSerializer
+
+#     def get_queryset(self):
+#         qs = Status.objects.all()
+#         query = self.request.GET.get('q')
+#         if query is not None:
+#             qs = qs.filter(content__icontains=query)
+#         return qs
+
+
+# class StatusCreateAPIView(generics.CreateAPIView):
+#     permission_classes = []
+#     authentication_classes = []
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+
+#     def perform_create(self, serializer):
+#         serializer.save(user=self.request.user)
+
+
+# class StatusDetailAPIView(generics.RetrieveAPIView):
+#     permission_classes = []
+#     authentication_classes = []
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
+#     lookup_field = 'id'
+
+#     def get_object(self, *args, **kwargs):
+#         kwargs = self.kwargs
+#         kw_id = kwargs.get('id')
+#         return Status.objects.get(id=kw_id)
 
 
 # class StatusUpdateAPIView(generics.UpdateAPIView):
